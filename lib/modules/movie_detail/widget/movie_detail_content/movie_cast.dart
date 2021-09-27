78dc0e736db9206806755ec33296332f2a409f4f
agrarcanjo <agr.arcanjo@gmail.com>
2021-09-27 20:03:23 -0300

    aula 5 dart week

---
+++ lib/modules/movie_detail/widget/movie_detail_content/movie_cast.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MovieCast extends StatelessWidget {
+
+  const MovieCast({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Container(width: 95,padding: EdgeInsets.all(10),child: ,),
+  }
+}
